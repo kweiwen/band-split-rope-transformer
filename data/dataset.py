420b47f10b4ac2b76d218f19c999b04690f7ebff
kweiwen <kweiwen.tseng@gmail.com>
2024-02-06 23:44:14 +0800

    refactor

--- data/dataset.py
+++ data/dataset.py
@@ -334,3 +334,143 @@ class moisesdbDataset(Dataset):
         self.silent_prob = silent_prob
         self.mix_prob = mix_prob
         self.mix_tgt_too = mix_tgt_too
+
+    def get_filelist(self) -> tp.List[tp.Tuple[str, tp.Tuple[int, int]]]:
+        filename2label = {}
+        filelist = []
+        i = 0
+        for line in tqdm(open(self.txt_path, 'r').readlines()):
+            file_name, start_idx, end_idx = line.split('\t')
+            if file_name not in filename2label:
+                filename2label[file_name] = i
+                i += 1
+            filepath_template = self.file_dir / "train" / f"{file_name}" / "{}.wav"
+            if self.preload_dataset:
+                mix_segment, tgt_segment = self.load_files(
+                    str(filepath_template), (int(start_idx), int(end_idx))
+                )
+                filelist.append((mix_segment, tgt_segment))
+            else:
+                filelist.append(
+                    (str(filepath_template), (int(start_idx), int(end_idx)))
+                )
+        return filelist
+
+    def load_file(
+            self,
+            file_path: str,
+            indices: tp.Tuple[int, int]
+    ) -> torch.Tensor:
+        assert Path(file_path).is_file(), f"There is no such file - {file_path}."
+
+        offset = indices[0]
+        num_frames = indices[1] - indices[0]
+        y, sr = torchaudio.load(
+            file_path,
+            frame_offset=offset,
+            num_frames=num_frames,
+            channels_first=True
+        )
+        assert sr == self.sr, f"Sampling rate should be equal {self.sr}, not {sr}."
+        if self.is_mono:
+            y = torch.mean(y, dim=0, keepdim=True)
+        return y
+
+    def load_files(
+            self, fp_template: str, indices: tp.Tuple[int, int],
+    ) -> tp.Tuple[torch.Tensor, torch.Tensor]:
+        mix_segment = self.load_file(
+            fp_template.format('mixture'), indices
+        )
+        tgt_segment = self.load_file(
+            fp_template.format(self.target), indices
+        )
+        max_norm = max(
+            mix_segment.abs().max(), tgt_segment.abs().max()
+        )
+        mix_segment /= max_norm
+        tgt_segment /= max_norm
+        return (
+            mix_segment, tgt_segment
+        )
+
+    @staticmethod
+    def imitate_silent_segments(
+            mix_segment: torch.Tensor,
+            tgt_segment: torch.Tensor
+    ) -> tp.Tuple[torch.Tensor, torch.Tensor]:
+        return (
+            mix_segment - tgt_segment,
+            torch.zeros_like(tgt_segment)
+        )
+
+    def mix_segments(
+            self,
+            tgt_segment: torch.Tensor,
+    ) -> tp.Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Creating new mixture and new target from target file and random multiple sources
+        """
+        # decide how many sources to mix
+        if not self.mix_tgt_too:
+            self.TARGETS.discard(self.target)
+        n_sources = random.randrange(1, len(self.TARGETS) + 1)
+        # decide which sources to mix
+        targets_to_add = random.sample(
+            self.TARGETS, n_sources
+        )
+        # create new mix segment
+        mix_segment = tgt_segment.clone()
+        for target in targets_to_add:
+            # get random file to mix source from
+            fp_template_to_add, indices_to_add = random.choice(self.filelist)
+            segment_to_add = self.load_file(
+                fp_template_to_add.format(target), indices_to_add
+            )
+            mix_segment += segment_to_add
+            if target == self.target:
+                tgt_segment += segment_to_add
+        return (
+            mix_segment, tgt_segment
+        )
+
+    def augment(
+            self,
+            mix_segment: torch.Tensor,
+            tgt_segment: torch.Tensor
+    ) -> tp.Tuple[torch.Tensor, torch.Tensor]:
+        if self.is_training:
+            # dropping target
+            if random.random() < self.silent_prob:
+                mix_segment, tgt_segment = self.imitate_silent_segments(
+                    mix_segment, tgt_segment
+                )
+            # mixing with other sources
+            if random.random() < self.mix_prob:
+                mix_segment, tgt_segment = self.mix_segments(
+                    tgt_segment
+                )
+        return mix_segment, tgt_segment
+
+    def __getitem__(
+            self,
+            index: int
+    ) -> tp.Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Each Tensor's output shape: [n_channels, frames_in_segment]
+        """
+        # load files
+        if self.preload_dataset:
+            mix_segment, tgt_segment = self.filelist[index]
+        else:
+            mix_segment, tgt_segment = self.load_files(*self.filelist[index])
+
+        # augmentations related to mixing/dropping sources
+        mix_segment, tgt_segment = self.augment(mix_segment, tgt_segment)
+
+        return (
+            mix_segment, tgt_segment
+        )
+
+    def __len__(self):
+        return len(self.filelist)
