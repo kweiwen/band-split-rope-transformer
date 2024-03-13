6b07d956ae7928e1088190c73a8688d08d7ca0b1
kweiwen.tseng <kweiwen.tseng@gmail.com>
2024-03-13 16:56:30 +0800

    use zip instead of loop iteration

--- data/utils.py
+++ data/utils.py
@@ -11,12 +11,7 @@ def collate_fn(
     Tensor's Input shape: Batch of [n_channels, n_timestamps]
     Tensor's Output shape: [batch_size, n_sources, n_channels, n_timestamps]
     """
-    batch_mix = []
-    batch_tgt = []
-
-    for mix, tgt in batch:
-        batch_mix.append(mix)
-        batch_tgt.append(tgt)
+    batch_mix, batch_tgt = zip(*batch)
     batch_mix = torch.stack(batch_mix, dim=0)
     batch_tgt = torch.stack(batch_tgt, dim=0)
     return torch.stack((batch_mix, batch_tgt), dim=1)
