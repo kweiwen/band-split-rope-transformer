f3552d6d2d079a718d8193bc936856b54b09e2dd
kweiwen.tseng <kweiwen.tseng@gmail.com>
2024-03-13 14:26:34 +0800

    add a concatenate function to get an unified dataset

--- train.py
+++ train.py
@@ -20,36 +20,48 @@ from utils.callbacks import ValidationProgressBar
 
 log = logging.getLogger(__name__)
 
+def create_dataset(select_dataset: DictConfig, cfg: DictConfig):
+    musdb_dataset = musdbDataset(
+        **cfg,
+    )
+    moisesdb_dataset = moisesdbDataset(
+        **cfg,
+    )
+
+    # map dataset class names to their instances. This allows for quick retrieval of dataset instances based on their class names.
+    datasets = {
+        type(musdb_dataset).__name__: musdb_dataset,
+        type(moisesdb_dataset).__name__: moisesdb_dataset
+    }
+
+    # prepare a temporary list to hold dataset instances that are activated in the configuration.
+    temp = []
+    for key, is_dataset_active in select_dataset.items():
+        # if a specific dataset is marked as active (True), append its instance to the temp list.
+        if is_dataset_active: temp.append(datasets.get(key))
+
+    # concatenate the selected datasets into a single dataset if multiple are activated.
+    # this provides a unified dataset interface for subsequent DataLoader operations.
+    return ConcatDataset(temp)
+
+
 def initialize_loaders(cfg: DictConfig) -> tp.Tuple[DataLoader, DataLoader]:
     """
     Initializes train and validation dataloaders from configuration file.
     """
-    train_musdb_dataset = musdbDataset(
-        **cfg.train_dataset,
-    )
-    train_moisesdb_dataset = moisesdbDataset(
-        **cfg.train_dataset,
-    )
-
-
-
+    train_dataset = create_dataset(cfg.multiple_dataset, cfg.train_dataset)
     train_loader = DataLoader(
-        train_moisesdb_dataset,
+        train_dataset,
         **cfg.train_loader,
         collate_fn=collate_fn
     )
+
     if hasattr(cfg, 'val_dataset'):
-        val_musdb_dataset = musdbDataset(
-            **cfg.val_dataset,
-        )
-        val_moisesdb_dataset = moisesdbDataset(
-            **cfg.val_dataset,
-        )
+        val_dataset = create_dataset(cfg.multiple_dataset, cfg.val_dataset)
         val_loader = DataLoader(
-            val_moisesdb_dataset,
+            val_dataset,
             **cfg.val_loader,
-            collate_fn=collate_fn
-        )
+            collate_fn=collate_fn)
     else:
         val_loader = None
     return (
