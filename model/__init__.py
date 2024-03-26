fc1313d84ef0ea656b0ddb70e35e70eb7e20d763
kweiwen.tseng <kweiwen.tseng@gmail.com>
2024-03-26 17:43:00 +0800

    rm unused part

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,3 @@
-from .bandsplitrnn import BandSplitRNN
 from .bs_roformer import BandSplitRoPETransformer
 from .attend import Attend
 from .mel_band_roformer import MelBandRoformer
--- model/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bandsplit import BandSplitModule
-from .bandsequence import BandSequenceModelModule
-from .maskestimation import MaskEstimationModule
-
-from .bandtransformer import BandTransformerModelModule
